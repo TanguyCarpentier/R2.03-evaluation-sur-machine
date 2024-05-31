3e5e9137dcd39f0720a3b3975db4568740288796
Tanguy <carpentiertanguy@gmail.com>
2024-05-31 14:15:44 +0200

    Ajoutez des fichiers projet.

---
+++ R2.03 évaluation sur machine/Program.cs
@@ -0,0 +1,14 @@
+﻿//CreeTab est un sous-programme qui ne prends aucun
+//paramètre. Il demande le nombre d'éléments 
+//harmoniques qui corresponds à la taille du tableau.
+//Et il créer et return un tableau qui corresponds à la taille
+//Indiquée.
+double[] CreeTab()
+{
+    int taille;
+    Console.WriteLine("Saisir le nombre d'éléments harmoniques");
+    taille=Convert.ToInt32(Console.ReadLine());
+    double[] tab=new double[taille];
+    return tab;
+}
+
